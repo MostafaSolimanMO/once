f21619c17c86772d003d449c9e2cb79b0bd42a64
SPiercer <snowpiercer2001@gmail.com>
2022-02-28 18:22:56 +0200

    update 2.0.0

--- README.md
+++ README.md
@@ -35,5 +35,7 @@ if (!rated){
 * [Mostafa Soliman](https://github.com/MostafaSolimanMO)
 * [Nour Magdi](https://github.com/SPiercer)
 
+## Please check the [Changelog](https://github.com/MostafaSolimanMO) for new updates
+
 
 inspired by the java library [Once](https://github.com/jonfinerty/Once) made by [Jon Finerty](https://github.com/jonfinerty)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,9 +18,7 @@ class _MyAppState extends State<MyApp> {
   String currentValue = 'Hello World';
 
   void set(String newOnce) {
-    setState(() {
-      currentValue = newOnce + ' ${Random().nextInt(100)}';
-    });
+    setState(() => currentValue = newOnce + ' ${Random().nextInt(100)}');
   }
 
   @override
@@ -42,75 +40,75 @@ class _MyAppState extends State<MyApp> {
                 ElevatedButton(
                     child: const Text("Run On New Version"),
                     onPressed: () {
-                      Once.runOnce("New Version", () {
-                        set("Hello New Version");
-                      });
+                      Once.runOnce(
+                          "New Version", () => set("Hello New Version"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Only Once"),
                     onPressed: () {
-                      Once.runOnce("Once", () {
-                        set("Hello Only Once");
-                      });
+                      Once.runOnce("Once", () => set("Hello Only Once"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Hourly"),
                     onPressed: () {
-                      Once.runHourly("Hourly", () {
-                        set("Hello Hourly");
-                      });
+                      Once.runHourly("Hourly", () => set("Hello Hourly"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Every 12 Hour"),
                     onPressed: () {
-                      Once.runEvery12Hours("12 Hour", () {
-                        set("Hello 12 Hour");
-                      });
+                      Once.runEvery12Hours(
+                          "12 Hour", () => set("Hello 12 Hour"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Daily"),
                     onPressed: () {
-                      Once.runDaily("Daily", () {
-                        set("Hello Daily");
-                      });
+                      Once.runDaily("Daily", () => set("Hello Daily"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Weekly"),
                     onPressed: () {
-                      Once.runWeekly("Weekly", () {
-                        set("Hello Weekly");
-                      });
+                      Once.runWeekly("Weekly", () => set("Hello Weekly"));
                     }),
                 ElevatedButton(
                     child: const Text("Run On New Month"),
                     onPressed: () {
-                      Once.runOnNewMonth("New Month", () {
-                        set("Hello New Month");
-                      });
+                      Once.runOnNewMonth<void>(
+                          "New Month", () => set("Hello New Month"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Monthly"),
                     onPressed: () {
-                      Once.runMonthly("Monthly", () {
-                        set("Hello Monthly");
-                      });
+                      Once.runMonthly<void>(
+                          "Monthly", () => set("Hello Monthly"));
                     }),
                 ElevatedButton(
                     child: const Text("Run Yearly"),
                     onPressed: () {
-                      Once.runYearly("Yearly", () {
-                        set("Hello Yearly");
-                      });
+                      Once.runYearly<void>("Yearly", () => set("Hello Yearly"));
                     }),
                 ElevatedButton(
-                    child: const Text("Run Evert 5 Sec"),
+                    child: const Text("Run Every 5 Sec"),
                     onPressed: () {
-                      Once.runCustom(
+                      Once.runCustom<void>(
                         "Custom",
                         () => set("Hello Custom"),
                         duration: const Duration(seconds: 5),
                       );
                     }),
+                FutureBuilder<Widget?>(
+                  future: Once.runCustom<Widget?>(
+                    "CustomWidget",
+                    () => Text('${Random().nextInt(100)}'),
+                    duration: const Duration(seconds: 5),
+                  ),
+                  builder: (_, snapshot) {
+                    if (snapshot.hasData) {
+                      return snapshot.data!;
+                    } else {
+                      return const Text("please come back after 5 seconds");
+                    }
+                  },
+                ),
                 const SizedBox(
                   height: 22,
                 ),
