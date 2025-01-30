440653ea2c153353cb9197e5e8d7058c8841754e
MohamedGawdat <moh.gawdt@gmail.com>
2025-01-30 18:36:47 +0300

    Update utils.dart
    -Remove un need func is"_leapYear"

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,3 +1,5 @@
+/// Utils class contains the helper methods that we depends
+/// on in the package
 abstract class Utils {
   /// Returns the number of days in a given month and year, considering leap years.
   static int daysInMonth(final int monthNum, final int year) {
@@ -9,8 +11,4 @@ abstract class Utils {
     return lastDayOfCurrentMonth.day;
   }
 
-  /// Checks if a given year is a leap year.
-  static bool _leapYear(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-  }
 }
