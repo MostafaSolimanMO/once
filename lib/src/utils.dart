437eaa72b1215dd47b79091d3877e4974242a95f
Mohamed Gawdat <moh.gawdt@gmail.com>
2026-04-19 16:28:05 +0300

    revert utils changes as it is in pr [31]

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,7 +3,12 @@
 abstract class Utils {
   /// Returns the number of days in a given month and year, considering leap years.
   static int daysInMonth(final int monthNum, final int year) {
-    return DateTime(year, monthNum + 1, 0).day;
-  }
+    // Create a DateTime object for the first day of the next month,
+    // then subtract one day to get the last day of the current month.
+    final firstDayOfNextMonth = DateTime(year, monthNum + 1, 1);
+    final lastDayOfCurrentMonth =
+        firstDayOfNextMonth.subtract(Duration(days: 1));
 
+    return lastDayOfCurrentMonth.day;
+  }
 }
